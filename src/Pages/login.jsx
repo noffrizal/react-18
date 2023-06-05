38476c363ccd1aaedf95dd427b3913101ef4c1e1
noffrizal <zaimjr@gmail.com>
2023-06-06 04:26:09 +0700

    implementation Link from react-router

--- src/Pages/login.jsx
+++ src/Pages/login.jsx
@@ -1,10 +1,15 @@
+import { Link } from "react-router-dom"
 import FormLogin from "../components/Fragments/FormLogin"
 import AuthLayouts from "../components/Layouts/AuthLayouts"
 
 const LoginPage = () => {
-    return(
+    return (
         <AuthLayouts title='Login'>
             <FormLogin />
+            <p className="text-sm mt-5 text-center">
+                Don't have an account?
+                <Link to="/register" className="font-bold text-blue-600">Sign Up here</Link>
+            </p>
         </AuthLayouts>
     )
 }
