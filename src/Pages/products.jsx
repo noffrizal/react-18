0123ce021320b4a3b242129ca968b155e039e735
noffrizal <zaimjr@gmail.com>
2023-06-27 09:32:02 +0700

    finishing Nested Components

--- src/Pages/products.jsx
+++ src/Pages/products.jsx
@@ -1,22 +1,19 @@
 import Button from "../components/Elements/Button"
+import CardProduct from "../components/Fragments/CardProduct"
 const ProductsPage = () => {
     return(
-        <div className="flex justify-center">
-            <div className="w-full max-w-sm bg-gray-800 border border-gray-700 rounded-lg shadow">
-                <a href="#">
-                    <img src="/images/shoes-1.jpg" alt="products" srcset="" className="p-8 rounded-t-lg" />
-                </a>
-                <div className="px-5 pb-5">
-                    <a href="http://">
-                        <h5 className="text-xl font-semibold tracking-tight text-white">Sepatu Baru</h5>
-                        <p className="text-s text-white">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Qui, velit. </p>
-                    </a>
-                </div>
-                <div className="flex items-center justify-between px-5 pb-5">
-                    <span className="text-xl font-bold text-white">Rp 1.000.000</span>
-                    <Button classname="bg-blue-600">Add to Chart</Button>
-                </div>
-            </div>
+        <div className="flex justify-center py-5">
+            <CardProduct>
+                <CardProduct.Header image="/images/shoes-1.jpg"/>
+                <CardProduct.Body title='Sepatu Baru'>LIni Adalah data septu merek apalah gitu </CardProduct.Body>
+                <CardProduct.Footer price = 'Rp 1.000.000' />
+            </CardProduct>
+
+            <CardProduct>
+                <CardProduct.Header image="/images/shoes-1.jpg"/>
+                <CardProduct.Body title='Sepatu Baru'>LIni Adalah data septu merek apalah gitu </CardProduct.Body>
+                <CardProduct.Footer price = 'Rp 1.000.000' />
+            </CardProduct>
         </div>
     )
 }
